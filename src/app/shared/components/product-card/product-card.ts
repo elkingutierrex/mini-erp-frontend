35c76e2eb1b04ee36ab66fad874f3c7c43f3ecf8
elkingutierrex <elkin.gutierrex@gmail.com>
2025-12-17 10:13:58 -0500

    feat: productCard

---
+++ src/app/shared/components/product-card/product-card.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  images?: string[];
+  price?: number;
+}
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.scss',
+})
+export class ProductCard {
+
+  @Input({ required: true }) product!: Product;
+
+  @Input() showAddButton = true;
+
+  @Output() addToCart = new EventEmitter<Product>();
+
+  onAddToCart() {
+    this.addToCart.emit(this.product);
+  }
+}
